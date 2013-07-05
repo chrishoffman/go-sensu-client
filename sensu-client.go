40aa37cd8271c51eaf80a987314317146538fb7a
Chris Hoffman <christopher.hoffman@gmail.com>
2013-07-05 11:55:48 -0400

    Initial rewrite

---
+++ sensu-client.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+	_ "strconv"
+	"github.com/streadway/amqp"
+)
+
+type SensuClient struct {
+	r *rabbitmq
+	k *keepalive
+}
+
+type rabbitmq struct {
+	uri     string
+	conn    *amqp.Connection
+	channel *amqp.Channel
+	connected chan bool
+	disconnected chan *amqp.Error
+}
+
+type keepalive struct {
+	interval time.Duration
+	timer    *time.Timer
+	restart  chan bool
+	stop     chan bool
+	close    chan bool
+}
+
+func (c *SensuClient) Start() chan error {
+
+    go c.r.connect()
+
+    for {
+        select {
+            case <- c.r.connected:
+            	c.keepalive(5 * time.Second)
+		}
+	}
+}
+
+func (r *rabbitmq) connect() {
+    var err error
+
+    retryTicker := time.NewTicker(10 * time.Second)
+    defer retryTicker.Stop()
+
+    done := make(chan bool)
+    go func() {
+        for _ = range retryTicker.C {
+            log.Printf("dialing %q", r.uri)
+            r.conn, err = amqp.Dial(r.uri)
+            if err != nil {
+                log.Println("Dial: %s", err)
+                continue
+            }
+
+            log.Printf("got Connection, getting Channel")
+            r.channel, err = r.conn.Channel()
+            if err != nil {
+                log.Println("Channel: %s", err)
+                continue
+            }
+
+            done <- true
+        }
+    }()
+    <-done
+
+    // Notify disconnect channel when disconnected
+    r.channel.NotifyClose(r.disconnected)
+
+    log.Println("RabbitMQ connected and channel established")
+    r.connected <- true
+}
+
+func (c *SensuClient) keepalive(interval time.Duration) {
+	c.k = &keepalive{
+		interval: interval,
+		restart:  make(chan bool),
+		stop:     make(chan bool),
+		close:    make(chan bool),
+	}
+
+	go c.k.loop(c.r)
+}
+
+func (k *keepalive) loop(r *rabbitmq) {
+	reset := make(chan bool)
+	k.timer = time.AfterFunc(k.interval, func() {
+		fmt.Println(time.Now())
+		reset <- true
+	})
+
+	for {
+		select {
+		case <-reset:
+			k.timer.Reset(k.interval)
+		case <-k.restart:
+			k.timer.Reset(k.interval)
+		case <-k.stop:
+			k.timer.Stop()
+		case <-k.close:
+			k.timer.Stop()
+			return
+		}
+	}
+}
+
+// func (k *keepalive) publish(t time.Time) {
+//     unixTimestamp := int64(t.Unix())
+//     msg := amqp.Publishing{
+//         ContentType:  "application/json",
+//         Body:         []byte(strconv.FormatInt(unixTimestamp, 10)),
+//         DeliveryMode: amqp.Persistent,
+//     }
+
+//     if err := c.channel.Publish(
+//         "keepalives",
+//         "",
+//         false,
+//         false,
+//         msg,
+//     ); err != nil {
+//         log.Printf("keepalive.publish: %v", err)
+//         return
+//     }
+//     log.Printf("Keepalive published: %s", strconv.FormatInt(unixTimestamp, 10))
+// }
+
+func main() {
+	r := &rabbitmq {
+		uri: "amqp://guest:guest@localhost:5672/",
+		connected:  make(chan bool),
+		disconnected:     make(chan *amqp.Error),		
+	}
+
+	c := &SensuClient {
+		r: r,
+	}
+
+	go c.Start()
+
+	time.Sleep(20 * time.Second)
+	panic("")
+
+}
