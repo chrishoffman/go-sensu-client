0a92f67d4c8dad1e1660fa7f6c7734cac148ce9e
Chris Hoffman <choffman@chathamfinancial.com>
2013-07-08 15:47:00 -0400

    go fmt

--- keepalive.go
+++ keepalive.go
@@ -1,91 +1,91 @@
 package main
 
 import (
-    "github.com/streadway/amqp"
-    "log"
-    "strconv"
-    "time"
+	"github.com/streadway/amqp"
+	"log"
+	"strconv"
+	"time"
 )
 
 type Keepalive struct {
-    r        *Rabbitmq
-    interval time.Duration
-    reset  chan bool
-    stop     chan bool
-    close    chan bool
+	r        *Rabbitmq
+	interval time.Duration
+	reset    chan bool
+	stop     chan bool
+	close    chan bool
 }
 
 func NewKeepalive(r *Rabbitmq, interval time.Duration) *Keepalive {
-    return &Keepalive{
-        r:        r,
-        interval: interval,
-        reset:  make(chan bool),
-        stop:     make(chan bool),
-        close:    make(chan bool),      
-    }
+	return &Keepalive{
+		r:        r,
+		interval: interval,
+		reset:    make(chan bool),
+		stop:     make(chan bool),
+		close:    make(chan bool),
+	}
 }
 
 func (k *Keepalive) Start() {
-    if err := k.r.channel.ExchangeDeclare(
-        "keepalives",
-        "direct",
-        true,
-        false,
-        false,
-        false,
-        nil,
-    ); err != nil {
-        log.Println("Exchange Declare: %s", err)
-    }
+	if err := k.r.channel.ExchangeDeclare(
+		"keepalives",
+		"direct",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		log.Println("Exchange Declare: %s", err)
+	}
 
-    k.publish(time.Now())
-    timer := time.AfterFunc(k.interval, func() {
-        k.publish(time.Now())
-        k.reset <- true
-    })
-    defer timer.Stop()
+	k.publish(time.Now())
+	timer := time.AfterFunc(k.interval, func() {
+		k.publish(time.Now())
+		k.reset <- true
+	})
+	defer timer.Stop()
 
-    for {
-        select {
-        case <-k.reset:
-            timer.Reset(k.interval)
-        case <-k.stop:
-            timer.Stop()
-        case <-k.close:
-            return
-        }
-    }
+	for {
+		select {
+		case <-k.reset:
+			timer.Reset(k.interval)
+		case <-k.stop:
+			timer.Stop()
+		case <-k.close:
+			return
+		}
+	}
 }
 
 func (k *Keepalive) Stop() {
-    k.stop <- true
+	k.stop <- true
 }
 
 func (k *Keepalive) Restart() {
-    k.reset <- true
+	k.reset <- true
 }
 
 func (k *Keepalive) Close() {
-    k.close <- true
+	k.close <- true
 }
 
 func (k *Keepalive) publish(timestamp time.Time) {
-    unixTimestamp := int64(timestamp.Unix())
-    msg := amqp.Publishing{
-        ContentType:  "application/json",
-        Body:         []byte(strconv.FormatInt(unixTimestamp, 10)),
-        DeliveryMode: amqp.Persistent,
-    }
+	unixTimestamp := int64(timestamp.Unix())
+	msg := amqp.Publishing{
+		ContentType:  "application/json",
+		Body:         []byte(strconv.FormatInt(unixTimestamp, 10)),
+		DeliveryMode: amqp.Persistent,
+	}
 
-    if err := k.r.channel.Publish(
-        "keepalives",
-        "",
-        false,
-        false,
-        msg,
-    ); err != nil {
-        log.Printf("keepalive.publish: %v", err)
-        return
-    }
-    log.Printf("Keepalive published: %s", strconv.FormatInt(unixTimestamp, 10))
+	if err := k.r.channel.Publish(
+		"keepalives",
+		"",
+		false,
+		false,
+		msg,
+	); err != nil {
+		log.Printf("keepalive.publish: %v", err)
+		return
+	}
+	log.Printf("Keepalive published: %s", strconv.FormatInt(unixTimestamp, 10))
 }
--- rabbitmq.go
+++ rabbitmq.go
@@ -1,88 +1,103 @@
 package main
 
 import (
-    "fmt"
-    "github.com/streadway/amqp"
-    "github.com/bitly/go-simplejson"
-    "log"
-    "net/url"
-    "strconv"
-    "time"
+	"fmt"
+	"github.com/bitly/go-simplejson"
+	"github.com/streadway/amqp"
+	"log"
+	"net/url"
+	"strconv"
+	"time"
 )
 
+type MessageQueuer interface {
+	// TODO: Fixing signature for config, needs to be just rmq settings
+	Connect(*simplejson.Json, chan bool, chan error)
+	ExchangeDeclare(string, string, bool, bool, bool, bool, amqp.Table) error
+}
+
 type Rabbitmq struct {
-    uri          string
-    conn         *amqp.Connection
-    channel      *amqp.Channel
-    disconnected chan *amqp.Error
+	uri          string
+	conn         *amqp.Connection
+	channel      *amqp.Channel
+	disconnected chan *amqp.Error
+}
+
+type RabbitmqConfig struct {
+	host     string
+	port     int64
+	vhost    string
+	user     string
+	password string
+	ssl      *tls.Config
 }
 
 const rabbitmqRetryInterval = 5 * time.Second
 
 func (r *Rabbitmq) Connect(cfg *simplejson.Json, connected chan bool, errc chan error) {
-    s, ok := cfg.CheckGet("rabbitmq")
-    if !ok {
-        errc <- fmt.Errorf("RabbitMQ settings missing from config")
-        return
-    }
+	s, ok := cfg.CheckGet("rabbitmq")
+	if !ok {
+		errc <- fmt.Errorf("RabbitMQ settings missing from config")
+		return
+	}
 
-    host := s.Get("host").MustString()
-    port := s.Get("port").MustInt()
-    user := s.Get("user").MustString()
-    password := s.Get("password").MustString()
-    vhost := s.Get("vhost").MustString()
+	host := s.Get("host").MustString()
+	port := s.Get("port").MustInt()
+	user := s.Get("user").MustString()
+	password := s.Get("password").MustString()
+	vhost := s.Get("vhost").MustString()
 
-    userInfo := url.UserPassword(user, password)
+	userInfo := url.UserPassword(user, password)
 
-    u := url.URL{
-        Scheme: "amqp",
-        Host:   host + ":" + strconv.FormatInt(int64(port), 10),
-        Path:   vhost,
-        User:   userInfo,
-    }
-    uri := u.String()
+	u := url.URL{
+		Scheme: "amqp",
+		Host:   host + ":" + strconv.FormatInt(int64(port), 10),
+		Path:   vhost,
+		User:   userInfo,
+	}
+	uri := u.String()
 
-    reset := make(chan bool)
-    done := make(chan bool)
-    go r.connect(uri, done)
-    timer := time.AfterFunc(rabbitmqRetryInterval, func() {
-        r.connect(uri, done)
-        reset <- true
-    })
-    defer timer.Stop()
-    
-    for {
-        select {
-        case <-done:
-            log.Println("RabbitMQ connected and channel established")
-            connected <- true
-            return
-        case <-reset:
-            timer.Reset(rabbitmqRetryInterval)
-        }
-    }
+	reset := make(chan bool)
+	done := make(chan bool)
+	go r.connect(uri, done)
+	timer := time.AfterFunc(rabbitmqRetryInterval, func() {
+		r.connect(uri, done)
+		reset <- true
+	})
+	defer timer.Stop()
+
+	for {
+		select {
+		case <-done:
+			log.Println("RabbitMQ connected and channel established")
+			connected <- true
+			return
+		case <-reset:
+			timer.Reset(rabbitmqRetryInterval)
+		}
+	}
 }
 
 func (r *Rabbitmq) connect(uri string, done chan bool) {
-    var err error
+	var err error
 
-    log.Printf("dialing %q", uri)
-    r.conn, err = amqp.Dial(uri)
-    if err != nil {
-        log.Printf("Dial: %s", err)
-        return
-    }
+	log.Printf("dialing %q", uri)
+	r.conn, err = amqp.Dial(uri)
+	if err != nil {
+		log.Printf("Dial: %s", err)
+		return
+	}
 
-    log.Printf("Connection established, getting Channel")
-    r.channel, err = r.conn.Channel()
-    if err != nil {
-        log.Printf("Channel: %s", err)
-        return
-    }
+	log.Printf("Connection established, getting Channel")
+	r.channel, err = r.conn.Channel()
+	if err != nil {
+		log.Printf("Channel: %s", err)
+		return
+	}
 
-    // Notify disconnect channel when disconnected
-    r.disconnected = make(chan *amqp.Error)
-    r.channel.NotifyClose(r.disconnected)
+	// Notify disconnect channel when disconnected
+	r.disconnected = make(chan *amqp.Error)
+	r.channel.NotifyClose(r.disconnected)
 
-    done <- true
+	done <- true
 }
