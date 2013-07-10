cc077c8b0fd4bd579cef323e96302e10a3af112f
Chris Hoffman <christopher.hoffman@gmail.com>
2013-07-09 22:00:09 -0400

    Beginning on subscriber

---
+++ sensu/subscriber.go
@@ -0,0 +1,17 @@
+package sensu
+
+import (
+	// "github.com/streadway/amqp"
+	// "log"
+	// "strconv"
+	// "time"
+)
+
+type Subscriber struct {
+	r MessageQueuer
+	s []subscription
+}
+
+type subscription struct {
+	s string
+}
