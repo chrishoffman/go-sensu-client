1ec50bd0c713fd103677eb8e6f915092ebfa1419
Chris Hoffman <christopher.hoffman@gmail.com>
2013-07-09 20:41:27 -0400

    Updating interface

--- update_mocks.sh
+++ update_mocks.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mockgen -package=mocks sensu-client/sensu Publisher >sensu/mocks/mock_publisher.go
+mockgen -package=mocks sensu-client/sensu MessageQueuer >sensu/mocks/mock_message_queuer.go
