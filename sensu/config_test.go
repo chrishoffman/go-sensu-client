d24eee52ed53cd1eebc9ad1976dea17b246de723
Chris Hoffman <choffman@chathamfinancial.com>
2013-07-11 14:04:54 -0400

    Config unit tests

--- sensu/config_test.go
+++ sensu/config_test.go
@@ -8,30 +8,6 @@ import (
 	"testing"
 )
 
-// func Test_Extend1(t *testing.T) {
-// 	d1 := make(map[string]interface{})
-// 	d2 := make(map[string]interface{})
-
-// 	d1["bbb"] = "blah"
-// 	d1["abc"] = []interface{}{"a", "b", "c"}
-// 	d2["abc"] = []interface{}{"a", "d"}
-
-// 	j1 := NewJson(d1)
-// 	j2 := NewJson(d2)
-
-// 	err := j1.Extend(j2)
-// 	if err != nil {
-// 		t.Error(err)
-// 	} else {
-// 		t.Log(fmt.Sprintf("%+v", j1))
-// 	}
-// }
-
-// func Test_Parse(t *testing.T) {
-// 	data, _ := parseFile("../config.json")
-// 	t.Log(data)
-// }
-
 func Test_Extend(t *testing.T) {
 	for _, tuple := range []struct {
 		src           string
