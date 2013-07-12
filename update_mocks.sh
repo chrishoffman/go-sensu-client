2ee649d5c5470e028a6b728f2ee138fd0935c7de
Chris Hoffman <christopher.hoffman@gmail.com>
2013-07-12 18:08:42 -0400

    Adding processor mock

--- update_mocks.sh
+++ update_mocks.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 mockgen -package=mocks sensu-client/sensu MessageQueuer >sensu/mocks/mock_message_queuer.go
+mockgen -package=mocks sensu-client/sensu Processor >sensu/mocks/mock_processor.go
